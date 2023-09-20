475767d0689a63d7be26d6ab40b82b64b413c3cb
4sum1 <asu.ropu41@gmail.com>
2023-09-20 20:03:57 +0900

    user folder added

---
+++ resources/views/users/private/group_list.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('title', 'Group list')
+
+@section('content')
+    {{-- @include() navbar here --}}
+
+    <div class="container mx-auto my-auto">
+        <h1 class="test-warning">Make a plan from place!</h1>
+        <p>map icon here</p>
+    </div>
+
+@endsection
+
+
+
