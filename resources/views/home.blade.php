5c17aa29eaff7a2fd4f3aa099bfb4ec0e2453c98
kotaromiyake.github.io <miyakou31@gmail.com>
2023-09-25 22:20:38 +0900

    created main page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+
+<style>
+.display-5 {
+    font-family: 'Marcellus', serif;
+}
+
+.p1 {
+    font-family: 'Marcellus', serif;
+}
+
+.p2 {
+    font-family: 'Muli', serif;
+}
+</style>
+
+<div class="background-image">
+ <div class="container display-flex align-items-center">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        {{-- <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('DASHBOARD') }}</div>
 
@@ -17,7 +33,26 @@
                     {{ __('YOU ARE LOGGED IN!') }}
                 </div>
             </div>
+        </div> --}}
+      <div><br><br><br><br><br></div>
+
+      <div class="card w-50 mt-5 text-center" style="background-color: rgba(230,242,233,0.75)">
+         <div>
+            <h1 class="display-5" style="color: #253C5C;">Make a plan</h1>
+            <p class="p1 h5" style="color: #617EA8;">Let's have food together!</p>
+         </div>
+        <div class="row mt-3">
+         <div class="link-date col">
+           <a href="#" class="link-date-icon"><i class="fa-solid fa-calendar-days fa-5x"></i>
+           <p class="p2 h5 mt-2">Decide by date</p></a>
+         </div>
+         <div class="link-place col">
+            <a href="#" class="link-place-icon"><i class="fa-solid fa-map-location-dot fa-5x"></i>
+            <p class="p2 h5 mt-2">Decide by place</p></a>
+         </div>
         </div>
+      </div>
     </div>
+ </div>
 </div>
 @endsection
