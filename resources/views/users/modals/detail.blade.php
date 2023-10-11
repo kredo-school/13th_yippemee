2b02b7f5567e88705113e9f2e00c5e512f8abec8
4sum1 <asu.ropu41@gmail.com>
2023-10-11 21:30:50 +0900

    i have created planList modal

--- resources/views/users/modals/detail.blade.php
+++ resources/views/users/modals/detail.blade.php
@@ -3,7 +3,7 @@
         <div class="modal-content">
 
             <div class="modal-header" id="detail-header">
-                <button type="button" class="close-detail border-0 bg-white" data-bs-dismiss="modal" aria-label="Close"><i class="fa-solid fa-x fa-2x" style="color:#253c5c;"></i></button>
+                <button type="button" class="close-detail border-0 bg-white" data-bs-toggle="modal" data-bs-target="#planList-modal"><i class="fa-solid fa-arrow-left fa-2x" style="color: #253c5c"></i></button>
                 <h4 class="modal-title" id="detailModalLabel">Looking for someone going to Italian with me!</h4>
 
             </div>
