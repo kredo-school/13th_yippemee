d747d4aa3cfc38afc61094cd795e6f97e0188369
megucchi <megumi082589@gmail.com>
2023-10-14 14:18:55 +0900

    change design

--- resources/views/users/profile/header.blade.php
+++ resources/views/users/profile/header.blade.php
@@ -1,30 +1,32 @@
-{{-- Header --}}
-<div class="row bg-white container shadow ms-auto me-auto mb-3 mt-4 rounded-3 p-5w">
-    <div class="col-5 mb-3 mt-3 main-font">
-            <i class="fa-regular fa-circle-user icon-md d-block text-end"></i>
+<div class="row bg-white container mx-auto mt-3" style="border-bottom: 3px solid lightgray">
+    <div class="col-6 mb-3 mt-3">
+        <i class="fa-regular fa-circle-user icon-md d-block text-end"></i>
     </div>
 
-    <div class="main-font col-5">
-        <div class="row">
-            <div class="col-auto">
-                <div class="display-6 mt-3">{{ "John Smith" }}
-                    <!-- Modal Button -->
-                    <a href="#" class="btn-sm mb-3 ms-3 icon-font">
-                        <i class="fa-solid fa-pen-clip icon-sm link-place" data-bs-toggle="modal" data-bs-target="#edit-intro"></i>
-                    </a>
-                </div>
+    <div class="col-3">
+        <div class="display-6 mt-3" style="font-family: 'Marcellus';">John Smith
+        </div>
+        <div class="header-user-name row">
+        <div class="mt-0">{{"@JohnSmith12345678"}}</div>
+        </div>
+    </div>
 
-                {{-- Include Modal --}}
-                @include('users.profile.modals.edit')
+    <div class="col-3 mt-4" style="text-align: right">
+         <a href="#" class="btn btn--green btn--cubic px-5">
+        <i class="fa-solid fa-pen-clip icon-sm" data-bs-toggle="modal" data-bs-target="#edit-intro"></i> Edit Profile
+        </a>
+    </div>
+        {{-- Include Modal --}}
+        @include('users.profile.modals.edit')
+
+        <div class="row">
+            <div class="col page-title" style="margin-left: 200px">
+                <span class="bar bar-short mt-4"></span>
             </div>
         </div>
 
-        <div class="header-user-name row">
-                <div class="mt-0">{{"@JohnSmith12345678"}}</div>
-        </div>
-    </div>
 
-    <nav class="profile-nav stroke">
+    <nav class="sidebarMenu profile-nav stroke">
           <ul>
             <li><a href="{{ route('posts.show') }}">POST</a></li>
             <li><a href="{{ route('visits.show') }}">VISITS</a></li>
--- resources/views/users/profile/side.blade.php
+++ resources/views/users/profile/side.blade.php
@@ -1,11 +1,11 @@
 {{-- side bar INTRO --}}
-<div class="bg-white container ms-auto me-auto rounded-3 p-5w">
+<div class="bg-white container ms-auto me-auto rounded-3 p-5w mt-3">
     <div class="row">
         <div class="col-3 mt-3">
             {{-- Intro --}}
-            <nav id="sidebarMenu" class="sidebarMenu collapse d-lg-block sidebar collapse bg-white shadow">
+            <nav id="sidebarMenu" class="sidebarMenu collapse d-lg-block sidebar collapse bg-white shadow ps-3 pt-2" style="padding-bottom: 20px">
                 <div class="mx-3 sidebar-profile">
-                    <div class="title-line"><i class="fa-regular fa-face-meh me-3"></i><span class="fw-bold">Intro</span></div>
+                    <div class="title-line"><i class="fa-regular fa-face-meh me-3 mt-3"></i><span class="fw-bold">Intro</span></div>
                     <a href="#" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
                     <i class="fa-solid fa-location-dot me-3 link-place" data-bs-toggle="modal" data-bs-target="#edit-intro"></i><span>Live</span>
                     </a>
