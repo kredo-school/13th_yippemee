c92ecb253d21cb6f7ac3e33c2087b5c5430b9140
4sum1 <asu.ropu41@gmail.com>
2023-10-17 21:51:13 +0900

    fixed forms position

--- resources/views/users/modals/add_group.blade.php
+++ resources/views/users/modals/add_group.blade.php
@@ -1,25 +1,12 @@
-<style>
-input,textarea{
-    border-bottom:2px solid #253c5c;
-    border-right:none;
-    border-left:none;
-    border-top:none;
-    width: 25rem
-}
-form input[type="file"] {
-    display: none;
-    cursor: pointer;
-}
 
-</style>
 <div class="modal fade" id="add-group" aria-hidden="true">
     <div class="modal-dialog">
-        <div class="modal-content">
+        <div class="modal-content" id="add-content">
             <div class="modal-header add-header bg-warning">
                 <div class="row w-100 align-items-center">
                     <div class="col-11 mt-2">
                         <h1 style="color: #253c5c;">
-                            <i class="fa-solid fa-square-plus fa-xl"></i>
+                            <i class="fa-regular fa-square-plus fa-xl"></i>
                             Create a group
                         </h1>
                     </div>
@@ -30,36 +17,34 @@ form input[type="file"] {
             </div>
 
 
-            <div class="modal-body">
+            <div class="modal-body" id="add-body">
                 <form action="#" method="post" enctype="multipart/form-data">
-                    <div class="form-group form-group-left">
-                        <label for="fileToUpload">
-                            <div class="profile-pic" style="background-image: url('https://randomuser.me/api/portraits/med/men/65.jpg')">
-                                <i class="fa-solid fa-camera"></i>
-                                <span>Edit Image</span>
-                            </div>
-                        </label>
-                        <input type="File" name="fileToUpload" id="fileToUpload">
-                    </div>
-
-                    <div class="form-group form-group-right">
-                        <label for="name"></label>
-                        <input type="text" placeholder="Name"  require>
-                        <br>
-                        <label for="restaurant"></label>
-                        <input type="text" placeholder="Restaurant" >
-                        <br>
-                        <label for="members" require></label>
-                        <input type="text" placeholder="Members" >
-                        <br>
-                        <label for="description"></label>
-                        <textarea name="description" id="description" cols="60" rows="5" placeholder="What is this group for?"></textarea>
+                    <div class="row" id="form-group">
+                        <div class="col-5" id="form-group-left">
+                            <label for="fileToUpload">
+                                <div class="profile-pic" style="background-image: url('https://randomuser.me/api/portraits/med/men/65.jpg')">
+                                    <i class="fa-solid fa-camera"></i>
+                                    <span>Edit Image</span>
+                                </div>
+                            </label>
+                            <input type="file" name="fileToUpload" id="fileToUpload">
+                        </div>
+                        <div class="col-6" id="form-group-right">
+                            <label for="name"></label>
+                            <input type="text" placeholder="Name"  require>
+                            <br>
+                            <label for="restaurant"></label>
+                            <input type="text" placeholder="Restaurant">
+                            <br>
+                            <label for="members" require></label>
+                            <input type="text" placeholder="Members">
+                        </div>
                     </div>
                 </form>
             </div>
 
             <div class="modal-footer border-0">
-                <a href="#" class="btn-create"><span>Create</span></a>
+                <a href="#" class="btn btn-create">Create</a>
             </div>
 
         </div>
