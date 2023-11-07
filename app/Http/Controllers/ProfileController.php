52748aa92313040923791fc239b24c8554d38cc4
masako kunii <m.wed.0701@gmail.com>
2023-11-07 22:20:26 +0900

    add posts to profile page.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\SocialPost;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
     public function show(){
-        return view('users.profile.show');
+        $social_posts = SocialPost::where('user_id', Auth::id())->latest()->get();
+        return view('users.profile.show', compact('social_posts'));
     }
 
     public function  edit(){
