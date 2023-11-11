c022a777ff1daa778e12ef3a73d2f59a67b2d2b8
4sum1 <asu.ropu41@gmail.com>
2023-11-11 17:04:51 +0900

    displayed year

--- resources/views/users/calendars/calendar.blade.php
+++ resources/views/users/calendars/calendar.blade.php
@@ -1,7 +1,10 @@
 <div class="monthly-calendar">
     <div class="w-100 calendar-button text-center">
-        <h1 id="monthName" class="text-center"></h1>
-        <div class="pre-next-button justify-content-center">
+        <div class="currentMonthYear">
+            <h1 id="monthName" class="text-center"></h1>
+            <h1 id="yearName" class="text-center"></h1>
+        </div>
+        <div class="prev-next-button justify-content-center">
             <button class="me-4 border-0 bg-white" id="prevMonth"><i class="fa fa-chevron-left fw-bold" style="color: #253c5c;"></i></button>
             <button class="ms-4 border-0 bg-white" id="nextMonth"><i class="fa fa-chevron-right" style="color: #253c5c;"></i></button>
         </div>
@@ -27,6 +30,7 @@
 
     <script>
         const calendarEl = document.getElementById('weekCalendar');
+        // get date,year,and month
         const date = new Date();
         let currentYear = date.getFullYear();
         let currentMonth = date.getMonth();
@@ -65,15 +69,31 @@
                         calendarHtml += '<tr>';
                     }
                 }
+
+
+                if (i === daysInMonth + startingDayOfWeek && i % 7 !== 0) {
+                    const remainingCells = 7 - (i % 7);
+                    for (let j = 0; j < remainingCells; j++) {
+                        calendarHtml += '<td></td>';
+                    }
+                    calendarHtml += '</tr>';
+                }
             }
             calendarHtml += '</tbody></table>';
 
+            // To dynamically generate a calendar on the page,
+                // allowing the user to navigate to the previous or next month.
             const monthNameElement = document.getElementById('monthName');
             monthNameElement.textContent = monthName;
-
             calendarEl.innerHTML = calendarHtml;
+
+            // year
+            const yearNameElement = document.getElementById('yearName');
+            yearNameElement.textContent = `${currentYear}`;
+
         }
 
+        // by click these button, display different month's calendar
         prevMonthBtn.addEventListener('click', () => {
             currentMonth--;
             if (currentMonth < 0) {
@@ -82,7 +102,6 @@
             }
             generateCalendar(currentYear, currentMonth);
         });
-
         nextMonthBtn.addEventListener('click', () => {
             currentMonth++;
             if (currentMonth > 11) {
