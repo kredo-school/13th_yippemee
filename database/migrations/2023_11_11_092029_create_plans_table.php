a1caf5363655db513206676d99afeff066ecc2bc
4sum1 <asu.ropu41@gmail.com>
2023-11-11 18:44:06 +0900

    no message

--- database/migrations/2023_11_11_092029_create_plans_table.php
+++ database/migrations/2023_11_11_092029_create_plans_table.php
@@ -15,6 +15,13 @@ class CreatePlansTable extends Migration
     {
         Schema::create('plans', function (Blueprint $table) {
             $table->id();
+            $table->string('date');
+            $table->decimal('time')->nullable();
+            $table->string('restaurant_id')->nullable();
+            $table->string('member_id');
+            $table->text('description');
+            $table->string('available_member')->nullable();
+
             $table->timestamps();
         });
     }
