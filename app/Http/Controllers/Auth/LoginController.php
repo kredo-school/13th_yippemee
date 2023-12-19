6ba7e4a9053ed6ac43a7ceeebd058d90c58a34fc
megucchi <megumi082589@gmail.com>
2023-12-19 22:38:26 +0900

    fixing logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -40,7 +40,6 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
-
     public function logout(Request $request)
     {
         $this->guard()->logout();
